579c809bfe9f16a48b4d155460eaa62c6f2d1c3f
Jon Kleiser <jon.kleiser@ceres.no>
2020-02-21 09:53:11 +0100

    TypeScript 3.8.2. Added repo info.

--- src/About.tsx
+++ src/About.tsx
@@ -11,6 +11,7 @@ export default class AboutComponent extends Component {
       <p>This Single Page Application combines functionality from <a href="#AppRun">AppRun</a> and <a href="#Classy-UI">Classy-UI</a>. In addition, TypeScript is used to avoid type errors and certain other errors in the code.</p>
       <p>To put together the necessary code and configuration including Webpack, Babel and TypeScript, it was very convenient to be able to build this project by extending Alexander Tarasyuk’s <a href="https://github.com/a-tarasyuk/webpack-typescript-babel" target="_blank">webpack-typescript-babel</a> repo.</p>
       <p>This is a very early version of this SPA, with not many interesting features yet.</p>
+      <p>The source code for this project can be found at <a href="https://github.com/jkleiser/apprun-classyui-spa" target="_blank">github.com</a>.</p>
       <p>This app. was built: {BundleVersion}</p>
     </div>
   }
